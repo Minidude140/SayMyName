efe527f2a5bcfad9c534e3f1b0b59aa7f55ac9c0
Zac <zac.minidude140@Gmail.com>
2023-08-29 08:10:25 -0600

    Added Console prompt and user input

--- SayMyName/SayMyName.vb
+++ SayMyName/SayMyName.vb
@@ -4,6 +4,11 @@ Option Strict On
 Module SayMyName
 
     Sub Main()
+        Dim name As String
+        Console.WriteLine("Hello, What is your name?")
+        name = Console.ReadLine()
+        Console.WriteLine($"Hello, {name}")
+        Console.Read()
 
     End Sub
 
