c5c8bc942947406893ff97fcef88c8d2cb4466bd
Zac <zac.minidude140@Gmail.com>
2023-08-29 08:17:07 -0600

    added comments

--- SayMyName/SayMyName.vb
+++ SayMyName/SayMyName.vb
@@ -5,9 +5,13 @@ Module SayMyName
 
     Sub Main()
         Dim name As String
+        'Console asks user for thier name
         Console.WriteLine("Hello, What is your name?")
+        'console sets variable "name" to user input and returns a promt
         name = Console.ReadLine()
-        Console.WriteLine($"Hello, {name}")
+        Console.WriteLine($"Hello, {name}.")
+        'console prompts user to close program
+        Console.WriteLine("Please push enter to quit program.")
         Console.Read()
 
     End Sub
